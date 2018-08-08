d0f85f550cb9bbbbdf25027fab9d97b76277cc00
Jesse Stimpson <jstimpson@republicwireless.com>
2018-08-08 08:51:22 -0400

    install files

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 readonly TGT=/home/jstimpson
 readonly DIRS=".config .tmuxp bin .ssh"
+readonly FILES=".tmux.conf"
 
 install_dir() {
     local dir
     dir="$1"
     echo "==> rsync-ing $dir to $TGT"
     rsync -av "$dir" "$TGT"
+}
+
+install_file() {
+    local file
+    file="$1"
+    echo "==> cp-ing $file to $TGT"
+    cp "$file" "$TGT"/"$file"
 }
 
 main() {
@@ -18,6 +26,11 @@ main() {
         do
             echo ""
             install_dir "$d"
+        done
+        for f in $FILES
+        do
+            echo ""
+            install_file "$f"
         done
     fi
 }
